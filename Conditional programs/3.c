ca364f53783b48867066cb542e2d1201b8bce355
Shifty Muhammad Ishmam <sishmam211040@bscse.uiu.ac.bd>
2022-10-21 12:41:20 +0600

    new file:   Conditional programs/1.c 	new file:   Conditional programs/1.exe 	new file:   Conditional programs/2.c 	new file:   Conditional programs/2.exe 	new file:   Conditional programs/3.c 	new file:   Conditional programs/3.exe new file:   Conditional programs/4.c 	new file:   Conditional programs/4.exe

---
+++ Conditional programs/3.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int number1, number2, number3, largeNumber;
+
+    printf("Number 1: ");
+    scanf(" %d", &number1);
+
+    printf("Number 2: ");
+    scanf(" %d", &number2);
+
+    printf("Number 3: ");
+    scanf(" %d", &number3);
+
+    largeNumber = number1 >= number2 ? (number1 >= number3 ? number1 : number3) : (number2 >= number3 ? number2 : number3);
+
+    printf("%d is the largest number.", largeNumber);
+    return 0;
+}
---
+++ Conditional programs/4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+int main()
+{
+    int number1, number2, number3, largeNumber;
+
+    printf("Number 1: ");
+    scanf(" %d", &number1);
+
+    printf("Number 2: ");
+    scanf(" %d", &number2);
+
+    printf("Number 3: ");
+    scanf(" %d", &number3);
+
+    if (number1 >= number2)
+    {
+        if (number1 >= number3)
+            printf("Number 1: %d is the largest number.", number1);
+        else
+            printf("Number 3: %d is the largest number.", number3);
+    }
+    else if (number2 >= number3)
+        printf("Number 2: %d is the largest number.", number2);
+    else
+        printf("Number 3: %d is the largest number.", number3);
+    return 0;
+}
